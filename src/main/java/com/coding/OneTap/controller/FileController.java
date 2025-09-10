4837f80ed4c4c3e4f30a500ac3f5dde497d31073
Encryption-prince <subhammaity6827@gmail.com>
2025-09-10 19:36:36 +0530

    cors fix 2

--- src/main/java/com/coding/OneTap/controller/FileController.java
+++ src/main/java/com/coding/OneTap/controller/FileController.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/files")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class FileController {
 
     private final FileService fileService;
